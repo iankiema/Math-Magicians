76ff1f51d6958219c2cdf3cbba7e60004a75c218
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-08-17 14:47:29 +0300

    Updated the components folder

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,65 +1,26 @@
 import React, { useState } from 'react';
 import './Calculator.css';
 import Button from './Button';
+import Calculate from './Calculate';
 
 const Calculator = () => {
-  const [displayValue, setDisplayValue] = useState('0');
-  const [prevValue, setPrevValue] = useState(null);
-  const [operator, setOperator] = useState(null);
-
-  const calculate = (num1, num2, op) => {
-    const value1 = parseFloat(num1);
-    const value2 = parseFloat(num2);
-
-    switch (op) {
-      case '+':
-        return value1 + value2;
-      case '-':
-        return value1 - value2;
-      case '*':
-        return value1 * value2;
-      case '/':
-        return value1 / value2;
-      default:
-        return value2;
-    }
-  };
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
   const handleButtonClick = (value) => {
-    if (value === 'C' || value === 'AC') {
-      // Clear the display and reset state
-      setDisplayValue('0');
-      setPrevValue(null);
-      setOperator(null);
-    } else if (['+', '-', '*', '/'].includes(value)) {
-      // Update prevValue and operator when an operator is pressed
-      setPrevValue(displayValue);
-      setOperator(value);
-      setDisplayValue('');
-    } else if (value === '=') {
-      // Perform calculation when '=' is pressed
-      if (prevValue && operator) {
-        const result = calculate(prevValue, displayValue, operator);
-        setDisplayValue(result.toString());
-        setPrevValue(null);
-        setOperator(null);
-      }
-    } else if (value === '.' && displayValue.includes('.')) {
-      // Avoid adding multiple decimal points
-
-    } else {
-      // Update display value
-      setDisplayValue((prevDisplay) => (prevDisplay === '0' || operator === prevDisplay ? value : prevDisplay + value));
-    }
+    const newCalculatorData = Calculate(calculatorData, value);
+    setCalculatorData(newCalculatorData);
   };
-
   const renderButton = (value, className) => (
     <Button key={value} value={value} className={className} onClick={handleButtonClick} />
   );
 
   return (
     <div className="calculator">
-      <div className="button output dark-gray">{displayValue}</div>
+      <div className="button output dark-gray">{calculatorData.next || calculatorData.total || '0'}</div>
       {renderButton('AC', 'button')}
       {renderButton('+/-', 'button')}
       {renderButton('%', 'button')}
